a5e0cfef881fc1d71e19557c43d9c3eeed2dcee6
owuordickson <owuordickson@gmail.com>
2022-03-16 20:42:37 +0300

    First running version

---
+++ py/comparison.py
@@ -0,0 +1,33 @@
+import cluster_gps as cgp
+import py.shared.so4gp as sgp
+from py.shared import config as cfg
+
+
+def compare_gps(clustered_gps, f_path, min_sup):
+    same_gps = []
+    miss_gps = []
+    str_gps, real_gps = sgp.graank(f_path, min_sup, return_gps=True)
+    for est_gp in clustered_gps:
+        check, real_sup = sgp.contains_gp(est_gp, real_gps)
+        # print([est_gp, est_gp.support, real_sup])
+        if check:
+            same_gps.append([est_gp, est_gp.support, real_sup])
+        else:
+            miss_gps.append(est_gp)
+    # print(same_gps)
+    print(str_gps)
+    return same_gps, miss_gps
+
+
+def run_comparison():
+    output, est_gps = cgp.clugps(f_path=cfg.DATASET, min_sup=cfg.MIN_SUPPORT, return_gps=True)
+    print(output)
+
+    # Compare inferred GPs with real GPs
+    hit_gps, miss_gps = compare_gps(est_gps, cfg.DATASET, cfg.MIN_SUPPORT)
+    d_gp = sgp.DataGP(cfg.DATASET, cfg.MIN_SUPPORT)
+    for gp in miss_gps:
+        print(gp.print(d_gp.titles))
+
+
+# run_comparison()
--- py/main.py
+++ py/main.py
@@ -1,33 +1,89 @@
-import cluster_gps as cgp
-import py.shared.so4gp as sgp
+# -*- coding: utf-8 -*-
+"""
+@author: "Dickson Owuor"
+@created: "16 Mar 2022"
+Usage:
+    $python comparison.py -f ../data/DATASET.csv -s 0.5
+Description:
+    f -> file path (CSV)
+    s -> minimum support
+    a -> clustering algorithm
+    e -> erasure probability
+    i -> maximum iteration
+"""
+
+import sys
+from optparse import OptionParser
 from py.shared import config as cfg
 
+if __name__ == "__main__":
+    if not sys.argv:
+        filePath = sys.argv[0]
+        minSup = sys.argv[1]
+        algChoice = sys.argv[2]
+        eProb = sys.argv[3]
+        itMax = sys.argv[4]
+        numCores = sys.argv[5]
+    else:
+        optparser = OptionParser()
+        optparser.add_option('-f', '--inputFile',
+                             dest='file',
+                             help='path to file containing csv',
+                             default=cfg.DATASET,
+                             type='string')
+        optparser.add_option('-s', '--minSupport',
+                             dest='minSup',
+                             help='minimum support value',
+                             default=cfg.MIN_SUPPORT,
+                             type='float')
+        optparser.add_option('-a', '--algorithmChoice',
+                             dest='algChoice',
+                             help='select algorithm for clustering',
+                             default=cfg.CLUSTER_ALGORITHM,
+                             type='string')
+        optparser.add_option('-e', '--eProb',
+                             dest='eProb',
+                             help='erasure probability',
+                             default=cfg.ERASURE_PROBABILITY,
+                             type='float')
+        optparser.add_option('-i', '--maxIteration',
+                             dest='itMax',
+                             help='maximum iteration for score vector estimation',
+                             default=cfg.SCORE_VECTOR_ITERATIONS,
+                             type='float')
+        optparser.add_option('-c', '--cores',
+                             dest='numCores',
+                             help='number of cores',
+                             default=cfg.CPU_CORES,
+                             type='int')
+        (options, args) = optparser.parse_args()
 
-def compare_gps(clustered_gps, f_path, min_sup):
-    same_gps = []
-    miss_gps = []
-    str_gps, real_gps = sgp.graank(f_path, min_sup, return_gps=True)
-    for est_gp in clustered_gps:
-        check, real_sup = sgp.contains_gp(est_gp, real_gps)
-        # print([est_gp, est_gp.support, real_sup])
-        if check:
-            same_gps.append([est_gp, est_gp.support, real_sup])
+        if options.file is None:
+            print("Usage: $python3 comparison.py -f filename.csv -a 'kmeans'")
+            sys.exit('System will exit')
         else:
-            miss_gps.append(est_gp)
-    # print(same_gps)
-    print(str_gps)
-    return same_gps, miss_gps
+            filePath = options.file
+        minSup = options.minSup
+        algChoice = options.algChoice
+        eProb = options.eProb
+        itMax = options.itMax
+        numCores = options.numCores
 
+    import time
+    import tracemalloc
+    import py.cluster_gps as cgp
+    from py.shared.profile import Profile
 
-def run_comparison():
-    output, est_gps = cgp.clugps(f_path=cfg.DATASET, min_sup=cfg.MIN_SUPPORT, return_gps=True)
-    print(output)
+    # CLU-GPs
+    start = time.time()
+    tracemalloc.start()
+    res_text = cgp.execute(filePath, minSup, algChoice, eProb, itMax, numCores)
+    snapshot = tracemalloc.take_snapshot()
+    end = time.time()
 
-    # Compare inferred GPs with real GPs
-    hit_gps, miss_gps = compare_gps(est_gps, cfg.DATASET, cfg.MIN_SUPPORT)
-    d_gp = sgp.DataGP(cfg.DATASET, cfg.MIN_SUPPORT)
-    for gp in miss_gps:
-        print(gp.print(d_gp.titles))
-
-
-# run_comparison()
+    wr_text = ("Run-time: " + str(end - start) + " seconds\n")
+    wr_text += (Profile.get_quick_mem_use(snapshot) + "\n")
+    wr_text += str(res_text)
+    f_name = str('res_cgp_v1' + str(end).replace('.', '', 1) + '.txt')
+    Profile.write_file(wr_text, f_name)
+    print(wr_text)
