4c114030a7d36550c9132223eb51af10071a8ade
owuordickson <owuordickson@gmail.com>
2022-04-23 11:51:20 +0300

    Added

--- src/main.py
+++ src/main.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
         wr_text += ("Memory Usage (MiB): " + str(mem_usage) + " \n")
         wr_text += str(res_text)
         f_name = str('res_clu' + str(end).replace('.', '', 1) + '.txt')
-        so4gp.write_file(wr_text, f_name, wr=False)
+        so4gp.write_file(wr_text, f_name, wr=True)
         print(wr_text)
     elif algChoice == 'acograd':
         # ACO-GRAANK
@@ -99,7 +99,7 @@ if __name__ == "__main__":
         wr_text += ("Memory Usage (MiB): " + str(mem_usage) + " \n")
         wr_text += str(res_text)
         f_name = str('res_aco' + str(end).replace('.', '', 1) + '.txt')
-        so4gp.write_file(wr_text, f_name, wr=False)
+        so4gp.write_file(wr_text, f_name, wr=True)
         print(wr_text)
     elif algChoice == 'graank':
         # GRAANK
@@ -112,7 +112,7 @@ if __name__ == "__main__":
         wr_text += ("Memory Usage (MiB): " + str(mem_usage) + " \n")
         wr_text += str(res_text)
         f_name = str('res_graank' + str(end).replace('.', '', 1) + '.txt')
-        so4gp.write_file(wr_text, f_name, wr=False)
+        so4gp.write_file(wr_text, f_name, wr=True)
         print(wr_text)
     else:
         print("Invalid Algorithm Choice!")
